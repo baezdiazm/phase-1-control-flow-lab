f3dbee7dbf754c34832c0032df27a9f9421ce6d0
baezdiazm <baezdiazm@yahoo.com>
2021-11-23 21:28:02 -0500

    commit tree c6784e48c7bb24ef929c1ebbdafaca9900c27add

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet > 2500) {
+    return `No can do.`
+  }
+  else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet <= 400) {
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // Write your code here!  
+  let message = ``
+  city === `NYC` ? message = `Ok, sounds good.` : message = `No go.`
+  return message
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case `generous`:
+      return `Thank you so much.`;
+      break;
+    case `not as generous`:
+      return `Thank you.`;
+      break;
+    default:
+      return `Bye.`;
+      break;
+  }
+
 }
